6b397e22765960608e667ce0b0ea690f9405e876
Paulaskub <poli.skubic@gmail.com>
2021-02-15 11:02:04 -0300

    Cambiando el estilo de los numeros del summary

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -17,13 +17,15 @@ return (
                     <div className= {info.color}>
                         <div className="card-body">
                             <div className="row no-gutters align-items-center">
-                                <div className="col mr-2">
-                                    <div className="text-xs font-weight-bold text-primary text-uppercase mb-1"> {info.titulo}</div>
-                                    <div className="h5 mb-0 font-weight-bold text-gray-800">{info.cifra}</div>
-                                </div>
                                 <div className="col-auto">
                                     <i className= {info.icono} ></i>
                                 </div>
+                                <div className="col mr-2">
+                                   
+                                    <div className="cantidades mb-0 font-weight-bold text-gray-800">{info.cifra}</div>
+                                    <div className="text-xs font-weight-bold text-primary text-uppercase mb-1"> {info.titulo}</div>
+                                </div>
+                                
                             </div>
                         </div>
                     </div>
