dadc697596be6a35430540742204296492bb1c5a
Paulaskub <poli.skubic@gmail.com>
2021-02-15 10:40:43 -0300

    Cambiando el titulo

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -7,8 +7,8 @@ return (
 
     <div className="pageheader_1">
 
-        <div className="d-sm-flex align-items-center justify-content-between mb-4">
-						<h1 className="h3 mb-0 text-gray-800">Reegalo Dashboard</h1>
+        <div className="titulo">
+						<h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
 		</div>
 
     </div>
