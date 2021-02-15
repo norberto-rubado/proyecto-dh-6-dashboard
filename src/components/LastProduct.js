db75d7d5f1b42d0b089d7408494048869a761613
Paulaskub <poli.skubic@gmail.com>
2021-02-15 11:42:30 -0300

    Cambiando estilo del ultimo producto

--- src/components/LastProduct.js
+++ src/components/LastProduct.js
@@ -17,22 +17,26 @@ function LastProduct() {
 
 return (
 
-    <div className="lastproduct_1">
+    <div className="lastproduct_1 container-card">
 
-        <div className="col-lg-6 mb-4">
-            <div className="card shadow mb-4">
+
+
+        <div className="card shadow mb-4 " >
                 <div className="card-header py-3">
                     <h6 className="m-0 font-weight-bold text-primary">Ultimo Producto Actualizado</h6>
                 </div>
-                <div className="card-body">
-                    <div className="text-center">
-                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4"  src={lastProduct.image} alt="dummy"/>
+                <div className="card-body card-props">
+                    <img className="img-last-product"  src={lastProduct.image} alt="dummy"/>
+                    <div className="lastproduct-detail">
+                        <p className="lastproduct-name">{lastProduct.name}</p>
+                        <p className="lastproduct-description">{lastProduct.description}</p>
+                        <p className="lastproduct-price">{lastProduct.price}</p>
                     </div>
-                    <p>{lastProduct.description}</p>
-                    <a target="_blank" rel="nofollow" href="/">Ver detalle del producto</a>
                 </div>
-            </div>
+
         </div>
+
+       
 
     </div>
 )}
